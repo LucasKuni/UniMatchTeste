9eba4db45a2a25956ed4101739371d3646958e8b
erickascencio <erickascencioo@hotmail.com>
2023-03-24 04:29:53 -0300

    firebase2

--- lib/cadastro_page.dart
+++ lib/cadastro_page.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:unimatchteste/Notificacao_page.dart';
 
@@ -11,46 +11,63 @@ class CadastroPage extends StatefulWidget {
   _CadastroPageState createState() => _CadastroPageState();
 }
 
+final FirebaseFirestore _db = FirebaseFirestore.instance;
+
 class _CadastroPageState extends State<CadastroPage> {
   int counter = 0;
-  String email = '', DatNasc = '', Uni = '', nome = '', Telefone = '',
-  senha = '', ConfSenha = '', CPF = '', UniPref = '', Nu = '';
+  String email = '',
+      DatNasc = '',
+      Uni = '',
+      nome = '',
+      Telefone = '',
+      senha = '',
+      ConfSenha = '',
+      CPF = '',
+      UniPref = '',
+      Nu = '';
   //Null Nu = null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(                   // Molda a tela para se assemelhar a um App
-        centerTitle : true,
-        title : Text('Cadastro'),
+      appBar: AppBar(
+        // Molda a tela para se assemelhar a um App
+        centerTitle: true,
+        title: Text('Cadastro'),
       ),
-      body: SingleChildScrollView(  //permite dar Scroll na tela enquanto digita
-        child: SizedBox(            //Box de Login
+      body: SingleChildScrollView(
+        //permite dar Scroll na tela enquanto digita
+        child: SizedBox(
+          //Box de Login
           width: MediaQuery.of(context).size.width, // delimita o tamanho (X,Y)
           height: MediaQuery.of(context).size.height,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),  //Distancia da borda do celular
+            padding: const EdgeInsets.all(8.0), //Distancia da borda do celular
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     nome = text;
                   },
                   keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Nome',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     email = text;
                   },
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Email',
                     border: OutlineInputBorder(),
                   ),
@@ -66,100 +83,128 @@ class _CadastroPageState extends State<CadastroPage> {
                     border: OutlineInputBorder(),
                   ),
                 ),**/
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     Telefone = text;
                   },
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Telefone',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     DatNasc = text;
                   },
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Data de Nascimento',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     Uni = text;
                   },
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Universidade',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     UniPref = text;
                   },
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Universidade que mais lhe agrada',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     senha = text;
                   },
-                  obscureText:  true,  //deixa senha nao visivel
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  obscureText: true, //deixa senha nao visivel
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Senha',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                Container(height: 10,),
+                Container(
+                  height: 10,
+                ),
                 TextField(
-                  onChanged: (text){
+                  onChanged: (text) {
                     print(text);
                     ConfSenha = text;
                   },
-                  obscureText:  true,  //deixa senha nao visivel
-                  decoration: InputDecoration(      // coloca borda no textfield
+                  obscureText: true, //deixa senha nao visivel
+                  decoration: InputDecoration(
+                    // coloca borda no textfield
                     labelText: 'Confirmar senha',
                     border: OutlineInputBorder(),
                   ),
                 ),
+                Container(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (senha == ConfSenha) {
+                        // Adicionar dados ao Firestore
+                        FirebaseFirestore.instance.collection('usuarios').add({
+                          'nome': nome,
+                          'email': email,
+                          'cpf': CPF,
+                          'telefone': Telefone,
+                          'data_nascimento': DatNasc,
+                          'universidade': Uni,
+                          'universidade_preferida': UniPref,
+                          'senha': senha
+                        });
 
-                Container(height: 20,),
-                ElevatedButton(onPressed: (){
-                  if(senha == ConfSenha){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginPage()),
-                    );
-                  }else{
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => NotificacaoSenhaPage()),
-                    );
-                    }
-                    //RETORNAR ConfSenha COMO NULL,LIMPAR O Q FOI DIGITADO
-                    /**ConfSenha.replaceAllMapped( Nu, (match) => Nu,);**/
-                    /**setState(() {
-                      ConfSenha = null;
-                      return ConfSenha.nu;
-                    });**/
-                }, child: Icon(Icons.done)),
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NotificacaoSenhaPage()),
+                        );
+                      }
+                    },
+                    child: Icon(Icons.done)),
               ],
             ),
           ),
         ),
       ),
-
     );
   }
 }
